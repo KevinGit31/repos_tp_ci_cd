ba1a24b8031461b72a5ff87e0c79d781c00059d8
AdrienCeschin <adrien.ceschin.devops@gmail.com>
2021-06-03 14:40:20 +0200

    vagrantfile and provisions for the two main vms.

---
+++ Vagrantfile
@@ -0,0 +1,34 @@
+Vagrant.configure("2") do |config|
+
+	config.vm.define "enjenkins" do |enjenkins|
+		enjenkins.vm.hostname = 'enjenkins'
+		enjenkins.vm.box = "ubuntu/bionic64"
+		enjenkins.vm.provider "virtualbox" do |vb|
+			vb.memory = 2048
+			vb.cpus = 1
+		end
+
+		enjenkins.vm.network "private_network", ip: "172.30.1.15"
+		enjenkins.vm.synced_folder "project/enjenkins", "/home/project", create: true
+
+		# Provisioning
+		enjenkins.vm.provision :shell, :path => "provision_jenkins.sh"
+ 
+	end
+
+	config.vm.define "envdeploy" do |envdeploy|
+		envdeploy.vm.hostname = 'envdeploy'
+		envdeploy.vm.box = "ubuntu/bionic64"
+		envdeploy.vm.provider "virtualbox" do |vb|
+			vb.memory = 2048
+			vb.cpus = 1
+		end
+
+		envdeploy.vm.network "private_network", ip: "172.30.1.16"
+		envdeploy.vm.synced_folder "project/envdeploy", "/home/project", create: true
+
+		# Provisioning
+		envdeploy.vm.provision :shell, :path => "provision_deploy.sh"
+	end
+ 
+end
---
+++ provision_deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+### MODE SECURE
+set -u # en cas de variable non définit, arreter le script
+set -e # en cas d'erreur (code de retour non-zero) arreter le script
+
+# Vérifier que le script est lancé en tant que root
+ps_assert_root(){
+	REAL_ID="$(id -u)"
+	if [ "$REAL_ID" -ne 0 ]; then
+		1>&2 echo "ERREUR: Le script doit etre exécuté en tant que root"
+		exit 1
+	fi
+}
+
+ps_check_install_packages() {
+    PACKAGE_NAME="$1"
+	apt update
+    if ! dpkg -l |grep --quiet "^ii.*$PACKAGE_NAME " ; then
+        apt install -y "$PACKAGE_NAME"
+    else
+        echo ""
+        echo "$PACKAGE_NAME est déjà installé."
+    fi
+}
+
+ps_install_kubernetes() {
+    ps_check_install_packages apt-transport-https
+	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+	ps_check_install_packages kubectl
+}
+
+ps_assert_root
+ps_install_kubernetes
